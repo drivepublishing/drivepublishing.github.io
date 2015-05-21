f9748d9f84369ce5d24d34f3cd17bd3210518802
Evan Turner <evturn@gmail.com>
2015-05-21 00:17:28 -0400

    landing page image and background adjusted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,1 @@
-var global = new Global();
-
 new WOW().init();
-
-
-function activatefullPage() {
-	$('.app-container').fullpage({paddingBottom: '250px'});
-}
-
-$(function() {
-
-	activatefullPage();
-
-});
