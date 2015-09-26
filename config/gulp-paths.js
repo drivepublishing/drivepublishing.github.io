4f24d4492558f230010d224ea9e93a092a84fb30
Evan Turner <evturn@gmail.com>
2015-09-25 23:53:27 -0400

    Add ESLint and Babel to gulp development

---
+++ config/gulp-paths.js
@@ -0,0 +1,57 @@
+module.exports = {
+  scss: {
+    src: 'assets/scss/*.scss',
+    watch: 'assets/scss/**/*.scss',
+    dest: 'dist/css',
+    filename: 'scss.css'
+  },
+  css: {
+    vendor: {
+      src: [
+      'assets/css/vendor/bootstrap.css',
+      'assets/css/vendor/font-awesome.css',
+      'assets/css/vendor/animate.css'
+      ],
+      dest: 'dist/css',
+      filename: 'vendor.css'
+    }
+  },
+  js: {
+    vendor: {
+      src: [
+        'assets/js/vendor/jquery.js',
+        'assets/js/vendor/bootstrap.js',
+        'assets/js/vendor/wow.js'
+      ],
+      dest: 'dist/js',
+      filename: 'vendor.js'
+    },
+    src: [
+      'assets/js/main.js'
+    ],
+    watch: [
+      'assets/js/**/*.js',
+      '!assets/js/vendor/**/*.js'
+    ],
+    dest: 'dist/js',
+    filename: 'scripts.js'
+  },
+  jshint: {
+    src: [
+      'assets/js/**/*.js',
+      '!assets/js/vendor/**/*.js',
+      'config/**/*.js',
+      'gulpfile.js'
+    ],
+    watch: [
+      'assets/js/**/*.js',
+      '!assets/js/vendor/**/*.js',
+      'config/**/*.js',
+      'gulpfile.js'
+    ]
+  },
+  img: {
+    src: 'assets/img/**/*',
+    dest: 'dist/img'
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp'),
+'use strict';
+
+let gulp = require('gulp'),
     gutil = require('gulp-util'),
-    g = require('gulp-load-plugins')();
-var options = require('./config/gulp-options');
-var paths = require('./config/paths');
+    $ = require('gulp-load-plugins')(),
+    options = require('./config/gulp-options'),
+    paths = require('./config/gulp-paths');
 
 // Default
 gulp.task('default', ['scss', 'lint', 'watch']);
@@ -15,46 +17,81 @@ gulp.task('watch', function() {
 // Build
 gulp.task('build', ['scss', 'scripts', 'js', 'css', 'img']);
 
+//////////////////////
+// BROWSERSYNC
+//////////////////////
+
+gulp.task('browserSync', function() {
+    browserSync.init(opts.browserSync);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('less:reload', ['less'], function() {
+    browserSync.reload();
+});
+
+gulp.task('js:reload', ['js'], function() {
+    browserSync.reload();
+});
+
+gulp.task('webpack:reload', ['webpack'], function() {
+    browserSync.reload();
+});
+
+
 gulp.task('scss', function() {
   return gulp.src(paths.scss.src)
-    .pipe(g.sass(options.scss))
-    .pipe(g.filesize())
-    .pipe(g.rename(paths.scss.filename))
+    .pipe($.sass(options.scss))
+    .pipe($.filesize())
+    .pipe($.rename(paths.scss.filename))
     .pipe(gulp.dest(paths.scss.dest))
     .on('error', gutil.log);
 });
 
 gulp.task('css', function() {
   return gulp.src(paths.css.vendor.src)
-    .pipe(g.plumber(options.plumber))
-    .pipe(g.concat(paths.css.vendor.filename))
-    .pipe(g.filesize())
-    .pipe(g.cssmin())
-    .pipe(g.rename(paths.css.vendor.filename))
-    .pipe(g.filesize())
-    .pipe(g.notify('vendor.css created'))
+    .pipe($.plumber(options.plumber))
+    .pipe($.concat(paths.css.vendor.filename))
+    .pipe($.filesize())
+    .pipe($.cssmin())
+    .pipe($.rename(paths.css.vendor.filename))
+    .pipe($.filesize())
+    .pipe($.notify('vendor.css created'))
     .pipe(gulp.dest(paths.css.vendor.dest));
 });
 
 gulp.task('js', function() {
   return gulp.src(paths.js.vendor.src)
-    .pipe(g.concat(paths.js.vendor.filename))
-    .pipe(g.filesize())
-    .pipe(g.uglify())
+    .pipe($.concat(paths.js.vendor.filename))
+    .pipe($.filesize())
+    .pipe($.uglify())
     .pipe(gulp.dest(paths.js.vendor.dest))
-    .pipe(g.filesize())
-    .pipe(g.notify('vendor.js created'))
+    .pipe($.filesize())
+    .pipe($.notify('vendor.js created'))
     .on('error', gutil.log);
 });
 
 gulp.task('scripts', function() {
   return gulp.src(paths.js.src)
-    .pipe(g.concat(paths.js.filename))
-    .pipe(g.filesize())
-    .pipe(g.uglify())
+    .pipe($.concat(paths.js.filename))
+    .pipe($.filesize())
+    .pipe($.uglify())
     .pipe(gulp.dest(paths.js.dest))
-    .pipe(g.filesize())
+    .pipe($.filesize())
     .on('error', gutil.log);
+});
+
+//////////////////////
+// ESLINT
+//////////////////////
+
+gulp.task('eslint', function() {
+  return gulp.src(paths.eslint.src)
+    .pipe($.plumber(opts.plumber))
+    .pipe($.eslint())
+    .on('error', opts.plumber.errorHandler)
+    .pipe($.eslint.format('stylish'))
+    .pipe($.notify(opts.notify.eslint));
 });
 
 gulp.task('lint', function() {
