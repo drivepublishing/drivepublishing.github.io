b715d911cec90f2d4a2cd36b2257ee3213cc5b86
Evan Turner <evturn@gmail.com>
2015-09-26 00:14:47 -0400

    Refactor gulpfile with new task structure

--- config/gulp-options.js
+++ config/gulp-options.js
@@ -1,4 +1,40 @@
 module.exports = {
+  browserSync: {
+    server: {
+      baseDir: './'
+    }
+  },
+  babel: {
+    modules: 'common'
+  },
+  plumber: {
+    errorHandler: function(err) {
+      gutil.beep();
+      console.log(err);
+      G.notify(err);
+      this.emit('end');
+    }
+  },
+  notify: {
+    eslint: function(file) {
+      if (file.eslint.errorCount === 0) {
+        return false;
+      }
+      let errors = file.eslint.messages.map(function(data) {
+        return '(' + data.line + ':' + data.column + ') ' + data.message;
+      }).join('\n');
+
+      return file.relative + ' (' + file.eslint.errorCount + ' errors)\n' + errors;
+    }
+  },
+  scss: {
+    sourceComments: 'map',
+    sourceMap: 'sass',
+    outputStyle: 'nested'
+  },
+  imagemin: {
+    progressive: true
+  },
   autoprefixer: {
     browsers: [
       '> 1%',
@@ -12,34 +48,5 @@ module.exports = {
       'IE 11'
     ],
     cascade: false
-  },
-  plumber: {
-    errorHandler: function(err) {
-      gutil.beep();
-      console.log(err);
-      G.notify(err);
-      this.emit('end');
-    }
-  },
-  notify: {
-    jshint: function(file) {
-      if (file.jshint.success) {
-        return false;
-      }
-      var errors = file.jshint.results.map(function(data) {
-        if (data.error) {
-          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
-        }
-      }).join("\n");
-      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
-    }
-  },
-  scss: {
-    sourceComments: 'map',
-    sourceMap: 'sass',
-    outputStyle: 'nested'
-  },
-  imagemin: {
-    progressive: true
   }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 'use strict';
 
-let gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    $ = require('gulp-load-plugins')(),
-    options = require('./config/gulp-options'),
-    paths = require('./config/gulp-paths');
+let gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    browserSync = require('browser-sync').create(),
+    $           = require('gulp-load-plugins')(),
+    paths       = require('./config/gulp-paths'),
+    opts        = require('./config/gulp-options');
 
-// Default
 gulp.task('default', ['scss', 'lint', 'watch']);
-//
-gulp.task('watch', function() {
-  gulp.watch(paths.scss.watch, ['scss']);
-  gulp.watch(paths.jshint.watch, ['lint']);
-  gulp.watch(paths.js.watch, ['scripts']);
+
+//////////////////////
+// WATCH
+//////////////////////
+
+gulp.task('less:watch', function() {
+  gulp.watch(paths.less.watch, ['less:reload']);
 });
-// Build
-gulp.task('build', ['scss', 'scripts', 'js', 'css', 'img']);
+
+gulp.task('js:watch', function() {
+  gulp.watch(paths.js.watch, ['js:reload']);
+});
+
+gulp.task('eslint:watch', function() {
+  gulp.watch(paths.eslint.watch, ['eslint']);
+});
+
+gulp.task('jshint:watch', function() {
+  gulp.watch(paths.jshint.watch, ['lint']);
+});
+
+gulp.task('webpack:watch', function() {
+  gulp.watch(paths.webpack.watch, ['webpack:reload']);
+});
 
 //////////////////////
 // BROWSERSYNC
@@ -38,46 +54,46 @@ gulp.task('webpack:reload', ['webpack'], function() {
     browserSync.reload();
 });
 
+//////////////////////
+// BABEL
+//////////////////////
+
+gulp.task('js', function() {
+  return gulp.src(paths.js.src)
+    .pipe($.plumber(opts.plumber))
+    .pipe($.sourcemaps.init())
+    .pipe($.babel(opts.babel))
+    .on('error', opts.plumber.errorHandler)
+    .pipe($.concat(paths.js.filename))
+    .pipe(gulp.dest(paths.dest.js))
+    .pipe($.uglify())
+    .pipe($.rename(paths.js.min))
+    .pipe(gulp.dest(paths.dest.js))
+    .pipe($.sourcemaps.write('.'))
+    .on('error', gutil.log);
+});
+
+//////////////////////
+// UGLIFY
+//////////////////////
+
+gulp.task('js:vendor', function() {
+  return gulp.src(paths.js.vendor.src)
+    .pipe($.plumber(opts.plumber))
+    .pipe($.concat(paths.js.vendor.filename))
+    .pipe(gulp.dest(paths.dest.js))
+    .pipe($.uglify())
+    .pipe($.rename(paths.js.vendor.min))
+    .pipe(gulp.dest(paths.dest.js))
+    .on('error', gutil.log);
+});
 
 gulp.task('scss', function() {
   return gulp.src(paths.scss.src)
+    .pipe($.plumber(opts.plumber))
     .pipe($.sass(options.scss))
-    .pipe($.filesize())
     .pipe($.rename(paths.scss.filename))
-    .pipe(gulp.dest(paths.scss.dest))
-    .on('error', gutil.log);
-});
-
-gulp.task('css', function() {
-  return gulp.src(paths.css.vendor.src)
-    .pipe($.plumber(options.plumber))
-    .pipe($.concat(paths.css.vendor.filename))
-    .pipe($.filesize())
-    .pipe($.cssmin())
-    .pipe($.rename(paths.css.vendor.filename))
-    .pipe($.filesize())
-    .pipe($.notify('vendor.css created'))
-    .pipe(gulp.dest(paths.css.vendor.dest));
-});
-
-gulp.task('js', function() {
-  return gulp.src(paths.js.vendor.src)
-    .pipe($.concat(paths.js.vendor.filename))
-    .pipe($.filesize())
-    .pipe($.uglify())
-    .pipe(gulp.dest(paths.js.vendor.dest))
-    .pipe($.filesize())
-    .pipe($.notify('vendor.js created'))
-    .on('error', gutil.log);
-});
-
-gulp.task('scripts', function() {
-  return gulp.src(paths.js.src)
-    .pipe($.concat(paths.js.filename))
-    .pipe($.filesize())
-    .pipe($.uglify())
-    .pipe(gulp.dest(paths.js.dest))
-    .pipe($.filesize())
+    .pipe(gulp.dest(paths.dest.css))
     .on('error', gutil.log);
 });
 
@@ -94,15 +110,12 @@ gulp.task('eslint', function() {
     .pipe($.notify(opts.notify.eslint));
 });
 
-gulp.task('lint', function() {
-  gulp.src(paths.jshint.src)
-    .pipe(g.plumber(options.plumber))
-    .pipe(g.jshint())
-    .pipe(g.notify(options.notify.jshint));
-});
+//////////////////////
+// IMAGEMIN
+//////////////////////
 
 gulp.task('img', function() {
   return gulp.src(paths.img.src)
-  .pipe(g.imagemin(options.imagemin))
-  .pipe(gulp.dest(paths.img.dest));
+  .pipe($.imagemin(opts.imagemin))
+  .pipe(gulp.dest(paths.dest.img));
 });
